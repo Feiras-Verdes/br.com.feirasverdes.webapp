897c2584541913554ba7583ebb8c0f71d70043d7
Jhully Muller <jhully.muller06@gmail.com>
2020-07-05 21:37:50 -0300

    ajuste na api de estande

--- src/api/estandes.api.js
+++ src/api/estandes.api.js
@@ -1,32 +1,43 @@
 import httpClient from "./httpClient.js";
 
-const endpointEstandes = process.env.VUE_APP_ENDPOINT_ESTANDES
+const endpointEstandes = process.env.VUE_APP_ENDPOINT_ESTANDES;
 
 //CRUD ESTANDE
-const cadastrarEstande = (estande) => httpClient.post(`${endpointEstandes}`, feira);
+const cadastrarEstande = (estande) =>
+  httpClient.post(`${endpointEstandes}`, estande);
 
-const atualizarEstande = (id, formData) => httpClient.put(`${endpointEstandes}/${id}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } });
+const atualizarEstande = (id, formData) =>
+  httpClient.put(`${endpointEstandes}/${id}`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
 
 const excluirEstande = (id) => httpClient.delete(`${endpointEstandes}/${id}`);
 
 const fetchEstande = (id) => httpClient.get(`${endpointEstandes}/${id}`);
 
 // NOTICIAS
-const fetchUltimasNoticias = () => httpClient.get(`${endpointEstandes}/ultimas-noticias`);
+const fetchUltimasNoticias = () =>
+  httpClient.get(`${endpointEstandes}/ultimas-noticias`);
 
-const fetchProdutosDeEstande = (id) => httpClient.get(`${endpointEstandes}/${id}/produtos`)
+const fetchProdutosDeEstande = (id) =>
+  httpClient.get(`${endpointEstandes}/${id}/produtos`);
 
-const fetchNoticiasDeEstande = (id) => httpClient.get(`${endpointEstandes}/${id}/noticias`)
+const fetchNoticiasDeEstande = (id) =>
+  httpClient.get(`${endpointEstandes}/${id}/noticias`);
 
-const avaliarEstande = (idUsuario, idEstande, nota) => httpClient.post(`${endpointEstandes}/${idEstande}/avaliar`, {idUsuario, nota});
+const avaliarEstande = (idUsuario, idEstande, nota) =>
+  httpClient.post(`${endpointEstandes}/${idEstande}/avaliar`, {
+    idUsuario,
+    nota,
+  });
 
 export {
-    fetchUltimasNoticias,
-    fetchEstande,
-    fetchProdutosDeEstande,
-    fetchNoticiasDeEstande,
-    avaliarEstande,
-    cadastrarEstande,
-    atualizarEstande,
-    excluirEstande
+  fetchUltimasNoticias,
+  fetchEstande,
+  fetchProdutosDeEstande,
+  fetchNoticiasDeEstande,
+  avaliarEstande,
+  cadastrarEstande,
+  atualizarEstande,
+  excluirEstande,
 };
